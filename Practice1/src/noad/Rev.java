e0be6812b8b51c56881e1b6163c399774848a075
admin <admin@DESKTOP-NKDGSPQ>
2022-11-06 19:56:19 +0530

    new repository

---
+++ Practice1/src/noad/Rev.java
@@ -0,0 +1,15 @@
+package noad;
+
+import java.util.HashMap;
+import java.util.Set;
+
+public class Rev {
+	public static void main(String [] args) {
+		String a [] = {"sushant","jadhav","padmaja","Ranada","prashant","jadhav"};
+		int b = a.length-1;
+		
+		System.out.println(a[b]);	
+		
+		
+	}
+	}
